098c68c5e1a101721f16ef049f761e25e5a302e4
lika <likagograchadze15@gmail.com>
2025-11-27 15:40:32 +0400

    add if else, reservetion.map,and show reservetion in browser

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
-var reservation = document.querySelector(".reservetion");
+var reservation = document.querySelector(".reservation");
 var reservations = [
-  {
-    roomname: "Standard King Room with Forest View",
-    guestname: "Alexandra Kasarkina",
-    checkin: "2021-03-08",
-    checkout: "2021-03-08",
-    payment: 2500,
-    currency: "GEL",
-    number: 12348,
-    status: "confrimed",
-  },
-  {
-    roomname: "Standard King Room with Forest View",
-    guestname: "Alexandra Kasarkina",
-    checkin: "2021-03-08",
-    checkout: "2021-03-08",
-    payment: 2500,
-    currency: "GEL",
-    number: 12348,
-    status: "confrimed",
-  },
-  {
-    roomname: "Standard King Room with Forest View",
-    guestname: "Alexandra Kasarkina",
-    checkin: "2021-03-08",
-    checkout: "2021-03-08",
-    payment: 2500,
-    currency: "GEL",
-    number: 12348,
-    status: "confrimed",
-  },
+    {
+        roomname: "Standard King Room with Forest View",
+        guestname: "Alexandra Kasarkina",
+        checkin: "2021-03-08",
+        checkout: "2021-03-08",
+        payment: 2500,
+        currency: "GEL",
+        number: 12348,
+        status: "confrimed",
+    },
+    {
+        roomname: "Standard King Room with Forest View",
+        guestname: "Alexandra Kasarkina",
+        checkin: "2021-03-08",
+        checkout: "2021-03-08",
+        payment: 2500,
+        currency: "GEL",
+        number: 12348,
+        status: "confrimed",
+    },
+    {
+        roomname: "Standard King Room with Forest View",
+        guestname: "Alexandra Kasarkina",
+        checkin: "2021-03-08",
+        checkout: "2021-03-08",
+        payment: 2500,
+        currency: "GEL",
+        number: 12348,
+        status: "confrimed",
+    },
 ];
-console.log(reservations);
+console.log(reservations.map(function (res) { return console.log(res); }));
 if (reservation) {
-  reservation.innerHTML = reservations
-    .map(function (reservation) {
-      return '\n        <div class="card">\n          <h3>'
-        .concat(reservation.roomname, "</h3>\n          <p>Guest: ")
-        .concat(reservation.guestname, "</p>\n          <p>Check-in: ")
-        .concat(reservation.checkin, "</p>\n          <p>Check-out: ")
-        .concat(reservation.checkout, "</p>\n          <p>Payment: ")
-        .concat(reservation.payment, " ")
-        .concat(reservation.currency, "</p>\n          <p>Reservation #: ")
-        .concat(reservation.number, "</p>\n          <p>Status: ")
-        .concat(reservation.status, "</p>\n        </div>\n      ");
+    reservation.innerHTML = reservations
+        .map(function (reservation) {
+        return "\n       <div class=\"card-res\">\n          <h3>".concat(reservation.roomname, "</h3>\n          <p>Guest: ").concat(reservation.guestname, "</p>\n          <p>Check-in: ").concat(reservation.checkin, "</p>\n          <p>Check-out: ").concat(reservation.checkout, "</p>\n          <p>Payment: ").concat(reservation.payment, " ").concat(reservation.currency, "</p>\n          <p>Reservation #: ").concat(reservation.number, "</p>\n          <p>Status: ").concat(reservation.status, "</p>\n        </div>\n    \n      \n  ");
     })
-    .join("");
+        .join(" ");
+}
+else {
+    console.log("error ");
 }
--- script.ts
+++ script.ts
@@ -1,49 +1,68 @@
-const reservation = document.querySelector(".reservetion");
+const reservation = document.querySelector(".reservation") as HTMLDivElement;
+
 type Reservetion = {
-  roomname:string,
-  guestname:string,
-  checkin:string,
-  checkout:string
-  payment: number,
-  currency:string,
-  number:number,
-  status: "confrimed" | "pending" | "cencelled"
+  roomname: string;
+  guestname: string;
+  checkin: string;
+  checkout: string;
+  payment: number;
+  currency: string;
+  number: number;
+  status: "confrimed" | "pending" | "cencelled";
 };
 
 const reservations: Reservetion[] = [
-{
-  roomname:"Standard King Room with Forest View",
-  guestname:"Alexandra Kasarkina",
-  checkin:"2021-03-08",
-  checkout:"2021-03-08",
-  payment: 2500,
-  currency:"GEL",
-  number: 12348,
-  status:"confrimed"
+  {
+    roomname: "Standard King Room with Forest View",
+    guestname: "Alexandra Kasarkina",
+    checkin: "2021-03-08",
+    checkout: "2021-03-08",
+    payment: 2500,
+    currency: "GEL",
+    number: 12348,
+    status: "confrimed",
+  },
+  {
+    roomname: "Standard King Room with Forest View",
+    guestname: "Alexandra Kasarkina",
+    checkin: "2021-03-08",
+    checkout: "2021-03-08",
+    payment: 2500,
+    currency: "GEL",
+    number: 12348,
+    status: "confrimed",
+  },
+  {
+    roomname: "Standard King Room with Forest View",
+    guestname: "Alexandra Kasarkina",
+    checkin: "2021-03-08",
+    checkout: "2021-03-08",
+    payment: 2500,
+    currency: "GEL",
+    number: 12348,
+    status: "confrimed",
+  },
+];
+console.log(reservations.map((res) => console.log(res)));
 
-},
-{
-  roomname:"Standard King Room with Forest View",
-  guestname:"Alexandra Kasarkina",
-  checkin:"2021-03-08",
-  checkout:"2021-03-08",
-  payment: 2500,
-  currency:"GEL",
-  number: 12348,
-  status:"confrimed"
-
-},
-{
-  roomname:"Standard King Room with Forest View",
-  guestname:"Alexandra Kasarkina",
-  checkin:"2021-03-08",
-  checkout:"2021-03-08",
-  payment: 2500,
-  currency:"GEL",
-  number: 12348,
-  status:"confrimed"
-
-},
-
-]
-console.log(reservations)
+if (reservation) {
+  reservation.innerHTML = reservations
+    .map((reservation) => {
+      return `
+       <div class="card-res">
+          <h3>${reservation.roomname}</h3>
+          <p>Guest: ${reservation.guestname}</p>
+          <p>Check-in: ${reservation.checkin}</p>
+          <p>Check-out: ${reservation.checkout}</p>
+          <p>Payment: ${reservation.payment} ${reservation.currency}</p>
+          <p>Reservation #: ${reservation.number}</p>
+          <p>Status: ${reservation.status}</p>
+        </div>
+    
+      
+  `;
+    })
+    .join(" ");
+} else {
+  console.log("error ");
+}
